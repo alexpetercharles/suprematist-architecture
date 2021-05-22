8d14df091b6affeb8a96d0c3a9872f6440b1b210
lexiboi <hi@alexpetercharles.ch>
2021-05-23 00:06:34 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Sketch from './components/Sketch.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Sketch,
+  },
+});
+</script>
+
+<template>
+  <sketch />
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/Sketch.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<template>
+  <div />
+</template>
+
+<style lang="scss" scoped>
+</style>
